926f5f61c4d06a6bbfd79188fbdff0b9186c02c2
Omar Warraich <earthling986@gmail.com>
2021-04-20 03:00:59 +0500

    second commit

---
+++ build/precache-manifest.1a37bffa131a3498b8a3d6faa8028fc8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a065b3f71adb55cf7c04dc2d89fedf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1e17db0a1fa94086621",
+    "url": "/static/css/main.7e3905d2.chunk.css"
+  },
+  {
+    "revision": "001c3f1cc238a7f0a1ba",
+    "url": "/static/js/2.1768474f.chunk.js"
+  },
+  {
+    "revision": "10017bfb08c3f52cf1c70a46d09c7c96",
+    "url": "/static/js/2.1768474f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e17db0a1fa94086621",
+    "url": "/static/js/main.94e81060.chunk.js"
+  },
+  {
+    "revision": "27e277ca3f130b8f1fce",
+    "url": "/static/js/runtime-main.ad540005.js"
+  },
+  {
+    "revision": "5619abbd30747b5c5f24beff1ac34192",
+    "url": "/static/media/ai-min.5619abbd.png"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "820e1f8d0640c0d1b2b81a5d455c3061",
+    "url": "/static/media/bcnc-min.820e1f8d.png"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "e9207edea1f480f206fa47f1b8db978d",
+    "url": "/static/media/iot-min.e9207ede.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "942c81fd7c87df7da8c098906a29d3fe",
+    "url": "/static/media/up-arrow-circle.942c81fd.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
